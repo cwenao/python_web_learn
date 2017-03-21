2ebe1aa4ad3bf6adc705674770a4ee4dc2845b28
cwenao@126.com <cwenao@126.com>
2017-03-21 12:30:20 +0800

    update the 15th

--- base100/base100/base_100.py
+++ base100/base100/base_100.py
@@ -241,3 +241,24 @@ r = reduceNum(99)
 
 for x in r:
     print(x)
+
+# 15. 题目：利用条件运算符的嵌套来完成此题：学习成绩>=90分的同学用A表示，60-89分之间的用B表示，60分以下的用C表示。
+
+scores = [90,60,0]
+g = ['A','B','C']
+
+def compareTo(students):
+    for x in students:
+        for k,v in enumerate(scores):
+            if x >= v:
+                yield g[k]
+                break
+
+stu = [50,30,80,100]
+
+f = compareTo(stu)
+
+for x in f:
+    print(x)
+
+
