e5b3928308455c2643f7d4392408182e61037831
cwenao@126.com <cwenao@126.com>
2017-04-05 20:53:41 +0800

    update the 31

--- base100/base100/base_100.py
+++ base100/base100/base_100.py
@@ -545,3 +545,16 @@ xx = plalindrome(123211)
 
 print('this is the plaidrome: ',xx)
 
+
+# 31 请输入星期几的第一个字母来判断一下是星期几，如果第一个字母一样，则继续判断第二个字母。
+# 这个题目改为if elif else 的应用
+
+def ifelif(doSomething):
+    if doSomething == 'a':
+        print('this is a -------------->')
+    elif doSomething == 'b':
+        print('this is b --------------->')
+    else:
+        print('this is nothing ----------------->')
+
+ifelif('b')
