8702f6b8b8fe7a97cb2fb67910124f5cbbb9517b
cwenao@126.com <cwenao@126.com>
2017-04-10 22:02:28 +0800

    update the 35th

--- base100/base100/base_100.py
+++ base100/base100/base_100.py
@@ -580,6 +580,7 @@ print(s1)
 
 # 34 练习函数调用。
 
+
 def print_words(words):
     print('this is the words: %s ' % words)
     return words
@@ -589,3 +590,18 @@ def do_print():
         print_words(str(x)+'abc')
 
 do_print()
+
+
+# 35 题目：文本颜色设置。
+
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+print (bcolors.WARNING + "警告的颜色字体?" + bcolors.ENDC)
