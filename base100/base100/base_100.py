b85ceaa42ff6062ac99830e1835e1dad90e305f9
cwenao <chenwenhao@shiruxu.com>
2017-04-13 21:33:56 +0800

    update the 38th

--- base100/base100/base_100.py
+++ base100/base100/base_100.py
@@ -632,3 +632,18 @@ f = bubbing_sort(l)
 
 for x in f:
     print(x)
+
+# 38 求一个3*3矩阵对角线元素之和。求一个3*3矩阵对角线元素之和。
+
+def duijiao():
+    a = []
+    sum = 0.0
+    for i in range(3):
+        a.append([])
+        for j in range(3):
+            a[i].append(float(input("input num:\n")))
+    for i in range(3):
+        sum += a[i][i]
+    print(sum)
+
+duijiao()
