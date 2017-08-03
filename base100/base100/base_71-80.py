267ab4046fbf4eeea9bf02cdd4723f5acf9dfd6f
cwenao <cwenao@126.com>
2017-08-03 09:21:17 +0800

    72th ~ 75th

--- base100/base100/base_71-80.py
+++ base100/base100/base_71-80.py
@@ -29,3 +29,53 @@ def output_stu(stu):
 
 
 #72 创建一个链表
+
+def create_link():
+    pLink = []
+
+    for i in range(20):
+        pLink.append(i)
+
+    print('this is the pLink', pLink)
+
+create_link()
+
+
+#73 题目：反向输出一个链表。
+
+
+def create_reverse_link():
+    pLink = []
+    for i in range(20):
+        pLink.append(i)
+    pLink.reverse()
+    print('this is the pLink to reverse print: ', pLink)
+
+create_reverse_link()
+
+
+#74. 题目：列表排序及连接。
+
+
+def sorted_and_linked():
+
+    head = [1,3,2,5,9,110,67]
+    tail = [111,45,89]
+
+    head.sort()
+
+    print('this is sorted the head: ', head)
+
+    #head + tail 也可以
+    head.extend(tail)
+    print('this is to extend the head and tail :', head)
+
+sorted_and_linked()
+
+
+#75. 放松一下，算一道简单的题目
+# 简单的判断题不想写
+
+
+
+
