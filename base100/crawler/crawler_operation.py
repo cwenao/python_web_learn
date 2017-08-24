145bc978c1955acee055dff101a0ae78d9e654e9
cwenao <cwenao@126.com>
2017-08-24 09:24:35 +0800

    update the multiprocess for crawer

--- base100/crawler/crawler_operation.py
+++ base100/crawler/crawler_operation.py
@@ -2,6 +2,7 @@ from urllib import request
 import re
 from base100.crawler.model import model
 from base100.crawler import db_operation
+from multiprocessing.dummy import Pool
 
 
 def data_capture_common(target_url, pat):
@@ -30,7 +31,22 @@ def data_douban_provider_list():
     session.commit()
 
 
+def data_multi_douban_provider_list():
+    '''
+    多线程
+    :return:
+    '''
+    pool = Pool(4)
+    target_url = 'https://read.douban.com/provider/all'
+    pat = '<div class="name">(.*?)</div>'
+    ret = data_capture_common(target_url, pat)
+    session = db_operation.get_session()
+    session.execute(model.ProviderInfo.__table__.insert(), [{'provider_name': x} for x in ret])
+    session.commit()
+    pool.close()
+    pool.join()
 
 
 if __name__ == '__main__':
-    data_douban_provider_list()
+    #data_douban_provider_list()
+    data_multi_douban_provider_list()
